657e2f31acaeada44be4e733fa27e6ecad0bbfc2
Rizzuto <S02862429@ccc.ccofc.edu>
2022-08-24 14:55:48 -0600

    Hello world

---
+++ Hello World/src/helloWorld.java
@@ -0,0 +1,16 @@
+/**Braeden Rizzuto
+ * 
+ */Hello World
+
+
+public class helloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("hello world");
+	}
+
+}
